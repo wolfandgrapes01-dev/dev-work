eea3f85198203c9bfc7697026e137506da3903d0
wolfandgrapes01-dev <wolfandgrapes01@gmail.com>
2025-11-06 14:59:19 +0900

    HWND.py を更新

--- HWND.py
+++ HWND.py
@@ -10,3 +10,16 @@ LRESULT CMsgDlg::OnResponse(WPARAM wParam, LPARAM lParam) {
     if (pyCallback) pyCallback(val);  // 调用 Python 回调
     return 0;
 }
+
+
+
+import mfc_msg
+
+def callback(val):
+    print("Received response:", val)
+
+dlg = mfc_msg.MsgDlg()
+dlg.register_callback(callback)
+
+# 在 C++ 端通过 PostMessage 或 SendMessage 触发 WM_RESPONSE
+# Python 会在 OnResponse 被调用时收到回调
