a8bccc038f91250be8afe16a2a137de48a14e555
wolfandgrapes01-dev <wolfandgrapes01@gmail.com>
2025-11-06 14:58:06 +0900

    HWND.py を更新

--- HWND.py
+++ HWND.py
@@ -1,28 +1,12 @@
-import winmsg
-import ctypes
-import threading
-from time import sleep
+// MyWnd.cpp
+#include "MyWnd.h"
 
-hwnd = winmsg.create_window(ctypes.windll.kernel32.GetModuleHandleW(None))
+BEGIN_MESSAGE_MAP(CMsgDlg, CDialog)
+    ON_MESSAGE(WM_RESPONSE, &CMsgDlg::OnResponse)
+END_MESSAGE_MAP()
 
-def on_message(val):
-    print("收到消息:", val)
-
-winmsg.register_callback(on_message)
-
-# 消息循环需要在后台线程运行
-def message_loop():
-    import ctypes.wintypes as wintypes
-    user32 = ctypes.windll.user32
-    msg = wintypes.MSG()
-    while user32.GetMessageW(ctypes.byref(msg), 0, 0, 0) != 0:
-        user32.TranslateMessage(ctypes.byref(msg))
-        user32.DispatchMessageW(ctypes.byref(msg))
-
-threading.Thread(target=message_loop, daemon=True).start()
-
-# 模拟发送消息
-import time
-time.sleep(1)
-ctypes.windll.user32.PostMessageW(hwnd, 0x400+100, 123, 0)
-time.sleep(1)
+LRESULT CMsgDlg::OnResponse(WPARAM wParam, LPARAM lParam) {
+    int val = static_cast<int>(wParam);
+    if (pyCallback) pyCallback(val);  // 调用 Python 回调
+    return 0;
+}
